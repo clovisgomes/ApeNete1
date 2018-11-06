78ca7039e7bb1bf047d0a75e885a4c8936da6236
Clóvis Fernando Gomes <cfogomes@hotmail.com>
2018-11-06 20:00:02 -0200

    06-11 20h00

---
+++ WebApplication4/Data/Mesa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Data
+{
+    public class Mesa
+    {
+        public int MesaId {get;set;}
+
+        public string Nome { get; set; }
+    }
+}
